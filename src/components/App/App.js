58be556ff6aed20448e38d9c621ba4fd01fb875e
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2022-04-06 09:24:13 -0600

    Initial component build

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,22 @@
-import { Component } from 'react';
-import './App.css';
+import { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Sick Trick Wish List</h1>
-      </div>
-    );
-  }
+	constructor() {
+		super()
+		this.state = {
+			tricks: [],
+			error: '',
+		}
+	}
+
+	render() {
+		return (
+			<div className='App'>
+				<h1>Sick Trick Wish List</h1>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
