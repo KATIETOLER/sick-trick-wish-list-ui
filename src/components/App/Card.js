f41696dab1d7be2b4c4bece7bd78572333e09151
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2022-04-06 10:05:17 -0600

    Add stance

--- src/components/App/Card.js
+++ src/components/App/Card.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Card.css'
 
-const Card = ({ name, obstacle, tutorial, id }) => {
+const Card = ({ name, obstacle, tutorial, stance, id }) => {
 	return (
 		<div className='card'>
 			<h3>{name}</h3>
-			<p>{obstacle}</p>
+			<p>Stance: {stance}</p>
+			<p>Obstacle: {obstacle}</p>
 			<p>See tutorial:</p>
 			<a href={tutorial}>{tutorial}</a>
 		</div>
